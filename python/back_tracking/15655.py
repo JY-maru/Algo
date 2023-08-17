ae88eb4d0afca2e35850a311b99a898aa41afeb7
kjy2134 <jeongyeon2134@naver.com>
2023-08-18 02:00:28 +0900

    8/15:후위 표기식

---
+++ python/back_tracking/15655.py
@@ -0,0 +1,43 @@
+# n과m (6)
+
+'''
+문제
+N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
+
+N개의 자연수 중에서 M개를 고른 수열
+고른 수열은 오름차순이어야 한다.
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+'''
+
+## method
+def sol(k):
+    global n,m
+    if k == m:
+        for i in range(m):
+            print(seq[i], end=' ')
+        print()
+        return
+
+    for i in range(n):
+        if not used[i]:
+            seq[k] = elem[i]
+            if k > 0 and seq[k] < seq[k-1] : continue
+            used[i] = True
+            sol(k+1)
+            used[i] = False
+## input
+n,m = map(int, input().split())
+elem = sorted(list(map(int, input().split())))
+seq = [0]*n
+used = [False]*n
+
+## output 
+sol(0)
