ff5d1d5c72be8e6ae8466922de1f9d3c2ff5f361
Kim.JeongYeon <101051712+JY-maru@users.noreply.github.com>
2023-08-29 03:38:06 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -0,0 +1,9 @@
+def solution(k, d):
+    import math
+    answer = 0
+    
+    for x in range(0,d+1,k):
+        y =  int(math.sqrt(d**2-x**2))
+        answer += y//k + 1
+        
+    return answer
