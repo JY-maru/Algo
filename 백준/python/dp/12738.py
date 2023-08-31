904d8afccdc00307a36dface6529c4db613f9c1a
kjy2134 <jeongyeon2134@naver.com>
2023-09-01 02:10:04 +0900

    8/31 : 가장 긴 증가하는 부분수열 3

--- 백준/python/dp/12738.py
+++ 백준/python/dp/12738.py
@@ -13,4 +13,30 @@
 출력
 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
 '''
-print(-float('inf') < -1e9)
+
+## method
+def sol():
+    memo = [-float('inf')]
+
+    for elem in arr :
+        
+        if memo[-1] < elem : memo.append(elem)
+        else :
+            left,right = 0, len(memo)
+
+            while left < right:    
+                mid = (left+right)//2
+
+                if memo[mid] < elem:
+                    left = mid + 1
+                else :
+                    right = mid
+            memo[right] = elem
+    
+    return len(memo)-1
+            
+## input
+N = int(input())
+arr = list(map(int,input().split()))
+## output
+print(sol())
